9bad918cb73f0ab9b8c264c332e2b75fcd7dca55
josesousa123 <153511700+josesousa123@users.noreply.github.com>
2025-02-28 16:33:25 +0000

    Criado o projeto maven com o esqueleto da classe principal DemoORM.

---
+++ Maven/src/isep/eapli/demo_orm/DemoORM.java
@@ -0,0 +1,8 @@
+package isep.eapli.demo_orm;
+
+public class DemoORM {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Maven/src/main/java/pt/isep/ipp/dei/esoft/template/Main.java
@@ -0,0 +1,7 @@
+package pt.isep.ipp.dei.esoft.template;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
