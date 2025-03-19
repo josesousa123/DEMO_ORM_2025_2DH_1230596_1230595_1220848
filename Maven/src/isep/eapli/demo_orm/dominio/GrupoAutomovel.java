b345364cc1e6b342b2f1571fcd26b5379d6ff6a2
josem <1220848@isep.ipp.pt>
2025-03-19 16:23:41 +0000

    Criação do método alterarNumeroPortas da Classe GrupoAutomovel

--- Maven/src/isep/eapli/demo_orm/dominio/GrupoAutomovel.java
+++ Maven/src/isep/eapli/demo_orm/dominio/GrupoAutomovel.java
@@ -1,4 +1,18 @@
 package isep.eapli.demo_orm.dominio;
 
 public class GrupoAutomovel {
+    private int numeroPortas;
+
+    public GrupoAutomovel(int numeroPortas) {
+        this.numeroPortas = numeroPortas;
+    }
+
+    public void alterarNumeroPortas(int novoNumero) {
+        this.numeroPortas = novoNumero;
+        System.out.println("Número de portas alterado para: " + this.numeroPortas);
+    }
+
+    public int getNumeroPortas() {
+        return this.numeroPortas;
+    }
 }
