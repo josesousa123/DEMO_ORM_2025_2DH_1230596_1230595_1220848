bbd73088948d5f9f6f7e64402809b368720d12c7
josesousa123 <153511700+josesousa123@users.noreply.github.com>
2025-03-19 16:30:29 +0000

    Criação do método ... da Classe Automovel .

--- Maven/src/isep/eapli/demo_orm/dominio/Automovel.java
+++ Maven/src/isep/eapli/demo_orm/dominio/Automovel.java
@@ -1,4 +1,24 @@
 package isep.eapli.demo_orm.dominio;
 
 public class Automovel {
+    private int numeroKms;
+
+    // Construtor padrão
+    public Automovel() {
+        this.numeroKms = 0;
+    }
+
+    // Método para alterar o número de kms
+    public void alterarNumeroKms(int novoNumeroKms) {
+        if (novoNumeroKms >= 0) {
+            this.numeroKms = novoNumeroKms;
+        } else {
+            throw new IllegalArgumentException("Número de kms não pode ser negativo");
+        }
+    }
+
+    // Getter do número de kms
+    public int getNumeroKms() {
+        return numeroKms;
+    }
 }
